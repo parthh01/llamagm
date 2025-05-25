60300876d34f863483dc0a2a8b4bcfe4f9e269ef
parth <parthb@nyu.edu>
2025-05-25 08:36:47 +0000

    multi gpu grpo

--- learn.sh
+++ learn.sh
@@ -1,1 +1,1 @@
-python -m model.learn --model_name ./train_output-final --output_dir ./grpo_output --per_device_train_batch_size 16 --num_iterations 10 --games_per_iteration 16 --progressive
+python -m model.learn --model_path ./train_output-final --output_dir ./grpo_output --per_device_train_batch_size 16 --num_iterations 10 --games_per_iteration 16 --progressive
