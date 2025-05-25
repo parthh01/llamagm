5f8c69272df97509d85f31661a358177e10b5926
parthh01 <parthb@nyu.edu>
2025-05-25 08:05:30 +0000

    hyperbolic.xyz sucks

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 sudo apt install stockfish screen
 python -m venv .venv 
-source .venv/bin/activate 
-pip install -r requirements.txt
 
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #python -m model.train --model_name meta-llama/Llama-3.2-1B --output_dir ./train_output --per_device_train_batch_size 16
-accelerate launch model/train.py --model_name meta-llama/Llama-3.2-1B --output_dir ./train_output --per_device_train_batch_size 16 
+accelerate launch --config-file ./accel_config.yaml model/train.py --model_name meta-llama/Llama-3.2-1B --output_dir ./train_output --per_device_train_batch_size 16 
 
